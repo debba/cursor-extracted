c3d753a17d1f369ddedae8e0c012112c8d1d406b
Altareos <altareos27@gmail.com>
2024-10-21 09:52:59 +0200

    improve autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -8,6 +8,8 @@ version="$(grep ^path: latest-linux.yml|cut -d- -f2)"
 build="$(grep ^path: latest-linux.yml|cut -d- -f4)"
 sum="$(grep ^sha512: latest-linux.yml|cut -d' ' -f2|base64 -d|hexdump -v -e '/1 "%02x" ')"
 
+rm latest-linux.yml
+
 echo "${version}" "${build}" "${sum}"
 
 sed -r "s@^(_cursor_version=).+@\1${version}@g" -i PKGBUILD
@@ -15,3 +17,4 @@ sed -r "s@^(_cursor_build=).+@\1${build}@g" -i PKGBUILD
 sed -r "s@^(sha512sums.+=\(').+('\))@\1${sum}\2@g" -i PKGBUILD
 
 makepkg --printsrcinfo > .SRCINFO
+git status
