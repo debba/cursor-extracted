748d45a427ea713e7da15d23ca94395de92465ae
Andrea Debernardi <andrea@debbaweb.it>
2025-09-22 12:24:36 +0200

    readded autoupdate.sh, manualupdate.sh

---
+++ autoupdate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl 'https://cursor.com/api/download?platform=linux-x64&releaseTrack=latest' -o latest.json
+
+version="$(grep -Po '"version":"\K[^"]+' latest.json)"
+url="$(grep -Po '"downloadUrl":"\K[^"]+' latest.json)"
+
+rm latest.json
+
+echo $version $url
+
+sed -r "s@^(_cursor_version=).+@\1${version}@g" -i PKGBUILD
+sed -r "s@^(_cursor_url=).+@\1${url}@g" -i PKGBUILD
+makepkg --printsrcinfo > .SRCINFO
+git commit -am "${version}"
